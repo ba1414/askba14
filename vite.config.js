9ce456a63c52c8eea4f709fa8cc92a22ed9e3ef6
askba14 <e1562707@u.nus.edu>
2025-11-13 18:11:36 +0800

    Set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/askba14/',     // 👈 repo name on GitHub
   plugins: [react()],
   server: {
     port: 8000
   }
 })
+
